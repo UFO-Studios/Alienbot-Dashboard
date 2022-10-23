5f8e11f86c7fb9ddc3a1fa5679e609f298fd94ea
MDxWARRIORxOP <mohammedsiddiqui2009@gmail.com>
2022-10-23 22:08:00 +0530

    create a next app

---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AlienBot Dashboard</title>
+      </Head>
+
+      <div className={styles.image}>
+        <div className="center">
+          <h1 className="white">AlienBot Dashboard</h1>
+        </div>
+
+        <div className="ultraCenter">
+          <a href="https://thealiendoctor.com/addAlienBot" class="bn13">
+            Add to your server
+          </a>
+        </div>
+
+        <div className="ultraCenter">
+          <a href="/serverList" class="bn13">
+            Configure your bot
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/serverList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/serverList.module.css"
+
+export default class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Server List</title>
+        </Head>
+
+
+      </div>
+    );
+  }
+}
